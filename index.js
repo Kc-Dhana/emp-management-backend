106226d24469e199ada299c944be0c18361d74b0
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2025-06-08 22:03:44 +0530

    Connect employee routes to Express app

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
-//import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import employeeRouter from './routes/employeeRoutes'
 
 
 dotenv.config()
@@ -26,7 +26,7 @@ mongoose.connect(connectionString).then(     //use mogoose to connect db.
     )
 )
 
-//app.use("/api/users",usersRouter)           //userRouter.js eke routes tika use karanwa "/users" dala.use api for best paratice,req eketh(postman)add weenna one api kiyala
+app.use("/api/employees",employeeRouter) 
                               
 
 
