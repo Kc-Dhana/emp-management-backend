e1c22ee9e9cd48cecbc7c7a39e2a42aa7b787e89
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2025-06-08 21:29:59 +0530

    clean code and create db connection

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
-import jwt from 'jsonwebtoken'
+//import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import cors from 'cors'
 
 
 dotenv.config()
-
 
 const app = express()
 
@@ -16,33 +15,6 @@ app.use(cors())
 app.use(bodyParser.json())
 
 const connectionString = process.env.MONGO_URL;
-
-//midelware karanne req kohen hari enawa nam e req eka aran mona hari wenasak karala processing ekak karala elaga kenwata yawanwa
-
-app.use((req,res,next)=>{                                               // Authentication middelwere ekak(bodypaser wage but meka api hadanne)
-
-    const token = req.header("Authorization")?.replace("Bearer ", "")   //ena token eka save karagannwa, token eka enne req header ekem
-                                                                        //req.header("Authorization")req heder eke,autherixation eke monama hari 
-                                                                        //tiyenwadaw check karawa
-                                                                        //? tiyenwa nam bearer eke tiyene eka token variable ekata aran denewa
-  
-    if(token != null){                          //token tiyena ekak da kiyala balanwa
-      jwt.verify(token,process.env.JWT_KEY,                //secret passs + token dala balnwa verfiwenawa nam pahala functioneka run wenna one
-        (err,decoded)=>{                        //decode karaddi error awada, nattma decode unada kiyala balanwa
-        if(decoded != null){                    //decode una nam
-          req.user = decoded                    //req usewa dala yawanwa. next req ekat(userwa gatte apu token eka decode karala)
-          next()                  //remove .body.user
-        }else{                                  //error awoth
-          next()
-        }
-  
-      }
-    )
-    }else{
-      next()
-    }
-  
-  });
 
 mongoose.connect(connectionString).then(     //use mogoose to connect db.
     ()=>{
@@ -54,18 +26,8 @@ mongoose.connect(connectionString).then(     //use mogoose to connect db.
     )
 )
 
-app.use("/api/users",usersRouter)           //userRouter.js eke routes tika use karanwa "/users" dala.use api for best paratice,req eketh(postman)add weenna one api kiyala
+//app.use("/api/users",usersRouter)           //userRouter.js eke routes tika use karanwa "/users" dala.use api for best paratice,req eketh(postman)add weenna one api kiyala
                               
-app.use("/api/gallery",galleryItemRouter)
-
-app.use("/api/category",categoryRouter)
-
-app.use("/api/rooms",roomRouter)
-
-app.use("/api/bookings",bookingRouter)
-
-app.use("/api/feedback",feedbackRouter)
-
 
 
 app.listen(5000,(req,res)=>{
